0393e110661ad40ab554b6595de536d5fa75f213
jordy192 <jordylucas_1@hotmail.com>
2018-03-08 15:47:41 -0300

    re-enviando2

--- vendor/hcodebr/php-classes/src/DB/Sql.php
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -3,7 +3,7 @@ namespace Hcode\DB;
 
 class Sql {
 
-	const HOSTNAME = "server100.hostinger.com.br";
+	const HOSTNAME = "mysql.hostinger.com.br";
 	const USERNAME = "u202465510_admin";
 	const PASSWORD = "rev725";
 	const DBNAME = "u202465510_dbrev";
